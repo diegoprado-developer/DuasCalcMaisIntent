6b13aa75528f55977dd63d9a2123fa6ce4a83559
Diego Prado <diego.droiddeveloper@gmail.com>
2017-04-20 17:53:29 -0300

    Passagem de Parametro Realizado

--- app/src/main/java/com/example/aluno/trabalhinho/Cadastro.java
+++ app/src/main/java/com/example/aluno/trabalhinho/Cadastro.java
@@ -1,9 +1,12 @@
 package com.example.aluno.trabalhinho;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Cadastro extends AppCompatActivity {
 
@@ -20,13 +23,44 @@ public class Cadastro extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
         inicializa();
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nome = edtNome.getText().toString();
+                String nasc = edtNascimento.getText().toString();
+                String tel = edtTelefone.getText().toString();
+                String email = edtEmail.getText().toString();
+                if (nome.trim().isEmpty() || nasc.trim().isEmpty() || tel.trim().isEmpty() || email.trim().isEmpty()) {
+                    Toast.makeText(v.getContext(), "Campos em branco", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Intent intent = new Intent(v.getContext(), ExibirCadastro.class);
+                    intent.putExtra("NOME", edtNome.getText().toString());
+                    intent.putExtra("NASCIMENTO", edtNascimento.getText().toString());
+                    intent.putExtra("TELEFONE", edtTelefone.getText().toString());
+                    intent.putExtra("EMAIL", edtEmail.getText().toString());
+                    startActivity(intent);
+
+                }
+            }
+        });
+
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 
-    public void inicializa(){
+    public void inicializa() {
         edtNome = (EditText) findViewById(R.id.edtNome);
-        edtNome = (EditText) findViewById(R.id.edtData);
-        edtNome = (EditText) findViewById(R.id.edtTelefone);
-        edtNome = (EditText) findViewById(R.id.edtEmail);
+        edtNascimento = (EditText) findViewById(R.id.edtData);
+        edtTelefone = (EditText) findViewById(R.id.edtTelefone);
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
         btnEnviar = (Button) findViewById(R.id.btnEnviar);
         btnVoltar = (Button) findViewById(R.id.btnVoltar);
     }
--- app/src/main/java/com/example/aluno/trabalhinho/ExibirCadastro.java
+++ app/src/main/java/com/example/aluno/trabalhinho/ExibirCadastro.java
@@ -1,13 +1,48 @@
 package com.example.aluno.trabalhinho;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ExibirCadastro extends AppCompatActivity {
+
+    TextView txtDados;
+    Button btnVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exibir_cadastro);
+
+        txtDados = (TextView) findViewById(R.id.txtDados);
+        btnVoltar = (Button) findViewById(R.id.btnVoltar);
+
+        Intent intent = getIntent();
+
+        Bundle params = new Bundle();
+        params = intent.getExtras();
+        if(params != null){
+
+            String nome = getIntent().getStringExtra("NOME");
+            String data = getIntent().getStringExtra("NASCIMENTO");
+            String telefone = getIntent().getStringExtra("TELEFONE");
+            String email = getIntent().getStringExtra("EMAIL");
+
+            txtDados.setText("Nome: "+nome+
+                                "\nNascimento: "+data+
+                                "\nTelefone: "+telefone+
+                                "\nEmail: "+email);
+        }
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
